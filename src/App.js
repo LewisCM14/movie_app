449c62f11690eedf7f2854bf7e845f76edf20a83
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-08 16:03:15 +0100

    render movies on page load with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   //  The movies state, passed as a prop to the MoviesList component
@@ -12,9 +12,9 @@ function App() {
 
   // HTTP error state handler
   const [error, setError] = useState(null);
-  
+
   /**
-   * async fetchMoviesHandler function, uses the fetch method, 
+   * async fetchMoviesHandler function, uses the fetch method,
    * which points at the star wars API URL.
    * previous error state is cleared with setError upon a good HTTP request
    * TRY BLOCK
@@ -22,7 +22,7 @@ function App() {
    * fetch defaults to a GET request and returns a promise.
    * if the response is not ok throw error,
    * else 'await' the promise to be delivered in the response object,
-   * the json data returned from the star wars api, 
+   * the json data returned from the star wars api,
    * is then converted to a JavaScript object in this response,
    * 'await' this data conversion,
    * then map the data into the transformedMovies object,
@@ -31,57 +31,64 @@ function App() {
    * CATCH BLOCK
    * if the response is not okay sets the error state to the string value
    */
-   async function fetchMoviesHandler() {
-     setIsLoading(true);
-     setError(null);
-     try {
-       const response = await fetch("https://swapi.py4e.com/api/films");
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/films");
 
-       if (!response.ok) {
-         throw new Error("Something went wrong!");
-       }
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-       const data = await response.json();
+      const data = await response.json();
 
-       const transformedMovies = data.results.map((movieData) => {
-         return {
-           id: movieData.episode_id,
-           title: movieData.title,
-           openingText: movieData.opening_crawl,
-           releaseDate: movieData.release_date,
-         };
-       });
-       setMovies(transformedMovies);
-       setIsLoading(false);
-     } catch (error) {
-       setError(error.message);
-     }
-     setIsLoading(false);
-   }
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
 
-   let content = <p>Found no movies.</p>;
+  /**
+   * useEffect hook that points at the fetchMoviesHandler function,
+   * calling it on page load, same function passed as a dependency
+   * when the function alters the effect is loaded again
+   * the callback hook on fetchMoviesHandler prevents infinite loop
+   */
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-   if (movies.length > 0) {
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
-   }
+  }
 
-   if (error) {
+  if (error) {
     content = <p>{error}</p>;
-   }
+  }
 
-   if (isLoading) {
+  if (isLoading) {
     content = <p>Loading...</p>;
-   }
-
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
